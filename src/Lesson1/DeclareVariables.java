e8815d40b2e0a222fbf0b4cd66075d6cca21851e
wackyvert <wvinc.com@gmail.com>
2019-12-05 14:19:14 -0600

    aaaaaaaa

--- src/Lesson1/DeclareVariables.java
+++ src/Lesson1/DeclareVariables.java
@@ -4,6 +4,17 @@ public class DeclareVariables {
     public static void main(String args[]){
         String name = "Jake";
         int age = 15;
-
+        System.out.println("Jake" + "Ronald" + "Torgerson");
+        printUpperCase(name);
+    }
+    public static void printLowerCase(String input)
+    {
+       input = input.toLowerCase();
+       System.out.println(input);
+    }
+    public static void printUpperCase(String input)
+    {
+        input = input.toUpperCase();
+        System.out.println(input);
     }
 }
