6ef7d0f4e78e2658b479d399698e7f874ca76cc0
wackyvert <wvinc.com@gmail.com>
2019-12-05 13:49:28 -0600

    adding stuff

---
+++ src/Lesson1/DeclareVariables.java
@@ -0,0 +1,9 @@
+package Lesson1;
+
+public class DeclareVariables {
+    public static void main(String args[]){
+        String name = "Jake";
+        int age = 15;
+
+    }
+}
--- src/Unit2/Main.java
+++ src/Unit2/Main.java
@@ -4,10 +4,7 @@ public class Main {
     public static void main(String args[]) {
             BankAccount ba1 = new BankAccount("Jake", 1000000000);
             System.out.println(ba1.getBalance());
-
         }
-
-
     }
 
 
