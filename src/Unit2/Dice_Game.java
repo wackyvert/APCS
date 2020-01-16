e77f0c1b0d514d5ceb41b0a516d89b3540b693f6
Jake Torgerson <44759116+wackyvert@users.noreply.github.com>
2020-01-16 13:58:05 -0600

    DiceGame

--- src/Unit2/Dice_Game.java
+++ src/Unit2/Dice_Game.java
@@ -5,33 +5,36 @@ import java.util.Scanner;
 public class Dice_Game {
     static Scanner scan = new Scanner(System.in);
     static int startingMoney;
-    static boolean status = true;
     static int betMoney;
+    static String choice;
 
     public static void main(String args[]) {
+        boolean status = true;
         Dice dice1 = new Dice(6);
         Dice dice2 = new Dice(6);
         System.out.println("How much money do you want to start with?");
         startingMoney = scan.nextInt();
         while (status) {
-            System.out.println("How much do you want to bet on this roll?");
-            betMoney = scan.nextInt();
+            System.out.println("How much do you want to bet on this roll? If you wish to stop, please type \"0\".");
+            betMoney=scan.nextInt();
+            if (betMoney <= startingMoney && betMoney > 0) {
+
             int roll1 = dice1.roll();
             int roll2 = dice2.roll();
             if (roll1 == roll2 || roll1 + roll2 == 7 || roll1 + roll2 == 11) {
                 startingMoney = startingMoney +(betMoney * 2);
-                System.out.println("Congrats, you managed to win something! Balance: " + startingMoney+"\nYou rolled: "+roll1+" "+roll2);
-            } else {
-                System.out.println("You lost. Sorry. You rolled: " + roll1 + " " + roll2);
-            }
-            if (startingMoney > 0) {
-                System.out.println("Do you want to play again?");
-                String choice = scan.nextLine();
-                if (choice.equalsIgnoreCase("no")) {
-                    status = false;
-                }
 
+                System.out.println("Congrats, you managed to win something! Balance: " + startingMoney+"\nYou rolled: "+roll1+" "+roll2);
+                System.out.println("Do you want to play again?");
+                choice = scan.nextLine();
+            } else {
+                startingMoney-=betMoney;
+                System.out.println("You lost. Sorry. You rolled: " + roll1 + " " + roll2+"\nYour balance is: "+startingMoney);
+
+                    }
+            }
+            else if(betMoney==0){status=false;}
             }
         }
     }
-}
+
