9e9263fb30df8980c0a9d91a098d4eedc80124da
wackyvert <wvinc.com@gmail.com>
2020-01-21 13:33:13 -0600

    Style 4

---
+++ src/Methods/Style4_practice.java
@@ -0,0 +1,21 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Style4_practice {
+    public static void main (String args[]){
+        double length, width, area, perimiter;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the length");
+        length=scan.nextDouble();
+        System.out.println("What is the width");
+        width=scan.nextDouble();
+        System.out.println("The area of your room is "+getArea(width, length)+" and the perimiter is "+getPerimiter(width, length));
+    }
+    public static double getArea(double base, double height){
+        return base * height;
+    }
+    public static double getPerimiter(double base, double height){
+        return base+base+height+height;
+    }
+}
